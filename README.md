7e1beba8fa9b9c11fd9e3f1be1f3caa599fed6d3
Radhe Shyam <radheshyam@politicaldata.com>
2024-06-06 21:12:02 +0530

    Added readme

---
+++ README.md
@@ -0,0 +1,12 @@
+#### To run the application locally
+
+
+Run the Backend Parallely or use this deployed backend endpoint ```http
+   https://wanderon-u2jk.onrender.com/api/
+  
+```
+and run command 
+```http
+   npm install
+  
+```
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,2 @@
-export const URL = "http://localhost:8080/api/"  // OR USE "https://wanderon-u2jk.onrender.com/api/"
+export const URL = "http://localhost:8080/api/"  // OR "https://wanderon-u2jk.onrender.com/api/"
 export const TOKEN = 'jwtToken'
